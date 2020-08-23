9a67a654311a1cf3e37e34725027b923913ea4e8
Histamina <july.independiente@gmail.com>
2020-08-23 01:46:16 -0300

    Adding NavBar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './NavBar.scss';
-import CartIcon from '../Icons/CartIcon';
+
 
 function NavBar() {
     return(
         <nav>
             <ul>
                 <li>
-                    <NavLink to="/" activeClassName="activeLink">
+                    <NavLink exact to="/" activeClassName="activeLink" className="main-nav">
                         Home
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to="/products" activeClassName="activeLink">
+                    <NavLink exact to="/products" activeClassName="activeLink" className="main-nav">
                         Products
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to="/cart" activeClassName="activeLink">
-                        <CartIcon />
+                    <NavLink exact to="/cart" activeClassName="activeLink" className="main-nav">
+                        Cart
                     </NavLink>
                 </li>
             </ul>
         </nav>
     );
-};
+}
 
 export default NavBar;
