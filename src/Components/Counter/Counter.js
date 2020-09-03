ac5015582786602b60b0c393b4fa068b899f4ca1
Histamina <july.independiente@gmail.com>
2020-09-03 17:40:16 -0300

    Fixing counter issues

--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -13,21 +13,23 @@ class Counter extends React.Component {
     }
 
     onAdd = (event) => {
-        let currentValue = event.target.value;
-        this.setState({currentValue}); 
+        let counter = event.target.value;
+        this.setState({counter}); 
     }
 
     increment = () => {
         if(this.state.counter < 10) {
-            let counter = this.state.counter +1;
-            this.setState({counter});
+            this.setState((currentValue) => {
+                return {counter: currentValue.counter +1};
+            });
         }
     }
 
     decrement = () => {
         if(this.state.counter > 0) {
-            let counter = this.state.counter -1;
-            this.setState({counter});
+            this.setState((currentValue) => {
+                return {counter: currentValue.counter -1};
+            });
         }
     }
 
@@ -36,13 +38,13 @@ class Counter extends React.Component {
             <div className="container my-5">
                 <div className="row no-gutters justify-content-center">
                     <div className="card card-Wrapper col-md-7 py-3 d-flex flex-row justify-content-center">
-                        <div className="col-md-5">
+                        <div className="col-md-5 d-flex align-items-center">
                             <img src={cartCamera} className="card-img img-fluid shadow border" alt="Camera to buy"  height="250" />
                         </div>
                         <div className="col-md-7">
                             <div className="card-body">
                                 <h5 className="card-title text-center">Add your product to the cart</h5>
-                                <input className="card-inputValue d-flex mx-auto my-4" type="number"  min="0" max="15" onAdd={this.onAdd} value={this.state.counter} />
+                                <input className="card-inputValue d-flex mx-auto my-4" type="number"  min="0" max="15" onChange={this.onAdd} value={this.state.counter} />
                                 <div className="button-Wrapper d-flex justify-content-center">
                                     <button type="button" className="btn btn-warning d-inline-block mx-3 button-style" onClick={this.increment}>+</button>
                                     <button type="button" className="btn btn-warning d-inline-block mx-3 button-style decrement-button" onClick={this.decrement}>-</button>
